d5c487d5473ad8e17f9dc874c97d6bd9a66bfc42
SamaSir2003 <sourabhgaganrao@gmail.com>
2025-02-22 10:12:57 +0530

    Chnaged things for deployment

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if os.environ.get("RENDER"):
+    DEBUG = False  # Ensure this is False in production
+    ALLOWED_HOSTS = ["your-app-name.onrender.com"]  # Replace with your Render app URL
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
